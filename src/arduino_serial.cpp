30ad5951ff02d6341fccc9c19a4e9607ed228eda
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-01-15 00:12:49 +0100

    Update arduino_serial.cpp

--- src/arduino_serial.cpp
+++ src/arduino_serial.cpp
@@ -289,13 +289,13 @@ void ArduinoSerial::update()
 
     std::cout << in_bytes << std::endl;
 
-    char* dec_string;
+    char* dec_string = new char[this->msg_length_decoded];
 
     std::vector<std::uint8_t> ret = base64::decode(in_bytes, this->msg_length_encoded);
 
     std::copy(ret.begin(), ret.end(),dec_string);
 
-    std::cout << dec_string << std::endl;
+    std::cout << "Test" << std::endl;
 
     /*
     char* dec_msg = decode(in_bytes);
