a298ecf3d5c5c5b8f6bad847b899e66471272b2d
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-01-15 01:02:48 +0100

    Update arduino_serial.cpp

--- src/arduino_serial.cpp
+++ src/arduino_serial.cpp
@@ -136,10 +136,10 @@ char *ArduinoSerial::encode(char *data) const
     {
         message_data[i] = data[i];
     }
-    message_data[this->msg_length_decoded - 4] = ((uint32_t)checksum >> 0) & 0xFF;
-    message_data[this->msg_length_decoded - 3] = ((uint32_t)checksum >> 8) & 0xFF;
-    message_data[this->msg_length_decoded - 2] = ((uint32_t)checksum >> 16) & 0xFF;
-    message_data[this->msg_length_decoded - 1] = ((uint32_t)checksum >> 24) & 0xFF;
+    message_data[this->msg_length_decoded - 1] = ((uint32_t)checksum >> 0) & 0xFF;
+    message_data[this->msg_length_decoded - 2] = ((uint32_t)checksum >> 8) & 0xFF;
+    message_data[this->msg_length_decoded - 3] = ((uint32_t)checksum >> 16) & 0xFF;
+    message_data[this->msg_length_decoded - 4] = ((uint32_t)checksum >> 24) & 0xFF;
 
     std::string result;
     result = base64::encode(message_data, this->msg_length_decoded);
